3a5d90d3b7a4cfe04583efb5b6aacb3a3417c8da
Allaball <mullruslan@yandex.ru>
2021-01-14 12:40:05 +0500

    fix(): declaration types for electron

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -1,12 +1,12 @@
+import { DesktopCapturerSource } from 'electron';
 import RGBA from "../renderer/classes/rgba";
 
 declare global {
     interface ElectronAPI {
         getPrimaryDisplayId: () => string;
         sendUpdateColorsRequest: (colors: RGBA[]) => void;
-        getScreenSources: () => Promise<any>;
+        getScreenSources: () => Promise<DesktopCapturerSource[]>;
     }
-
     interface Window {
         electronApi?: ElectronAPI;
     }
