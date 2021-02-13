7fd077d15d503be8aea85f7316299ad0a96b34c6
Allaball <mullruslan@yandex.ru>
2021-02-13 13:01:54 +0500

    feat(): implemented packing a preload

---
+++ build/webpack.config.preload.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const base = require('./webpack.config');
+const { merge } = require('webpack-merge');
+
+module.exports = merge(base, {
+    context: path.resolve(__dirname, '..', 'src', 'main'),
+    target: 'electron-preload',
+    entry: './preload.js',
+    output: {
+        filename: 'preload.js',
+        path: path.resolve(__dirname, '..', 'dist'),
+        publicPath: '/'
+    },
+    externals: {
+        worker_threads: 'commonjs',
+    },
+    node: {
+        __dirname: false,
+    },
+});
